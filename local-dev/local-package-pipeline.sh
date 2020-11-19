9e75667f9b87d4718066a1ea0432c609b76aa60c
Micah Nagel <micah.nagel@parsons.com>
2020-11-19 11:22:34 -0700

    [BB-638] Make script more futureproof, error handling

--- local-dev/local-package-pipeline.sh
+++ local-dev/local-package-pipeline.sh
@@ -1,69 +1,33 @@
 #!/bin/bash
 # Runs the conftest stage of the package pipeline
-conftest() {
+conftest() {( set -e
     echo "Directory structure of repository:"
     tree $1
     echo "Generic configuration validation tests:"
-    if helm conftest $1/chart --policy ../policies ; then
-        if [ -d "$1/policy" ]; then
-            echo "App specific configuration validation tests:"
-            if helm conftest chart --policy $1/policy ; then
-                return 0
-            else
-                return 1
-            fi
-        fi
-    else
-        return 1
+    helm conftest $1/chart --policy ../policies
+    if [ -d "$1/policy" ]; then
+        echo "App specific configuration validation tests:"
+        helm conftest chart --policy $1/policy
     fi
-}
+)}
 
 # Runs the package test stage of the package pipeline
-packagetest() {
-    if [ -z $1 ]
-    then
-        echo "Please specify the app path as first argument."
-        exit 1
-    elif [[ $1 == *"../"* ]]
-    then
-        echo "Please use the absolute path."
-        exit 1
-    fi
+packagetest() {( set -e
+    k3d cluster create package-pipeline-devtest --k3s-server-arg "--disable=metrics-server" --k3s-server-arg "--disable=traefik" -p 80:80@loadbalancer -p 443:443@loadbalancer --wait --agents 1 --servers 1
+    while ! (kubectl get node | grep "agent" > /dev/null); do sleep 3; done
+    kubectl wait --for=condition=available --timeout 600s -A deployment --all > /dev/null
+    kubectl wait --for=condition=ready --timeout 600s -A pods --all --field-selector status.phase=Running > /dev/null
 
-    if k3d cluster create package-pipeline-devtest \
-           --k3s-server-arg "--disable=metrics-server" \
-           --k3s-server-arg "--disable=traefik" \
-           -p 80:80@loadbalancer \
-           -p 443:443@loadbalancer \
-           --wait --agents 1 --servers 1
-    then
-        while ! (kubectl get node | grep "agent" > /dev/null); do sleep 3; done
-        kubectl wait --for=condition=available --timeout 600s -A deployment --all > /dev/null
-        kubectl wait --for=condition=ready --timeout 600s -A pods --all --field-selector status.phase=Running > /dev/null
-
-        # Place kubernetes package test here
-        echo "Package install"
-        if helm install devtest $1/chart -n devtest --create-namespace -f $1/tests/test-values.yml ; then
-            kubectl wait --for=condition=available --timeout 600s -A deployment --all > /dev/null
-            kubectl wait --for=condition=ready --timeout 600s -A pods --all --field-selector status.phase=Running > /dev/null
-            echo "Package tests"
-            kubectl get ingress --all-namespaces
-            kubectl get all -A
-            if cypress verify ; then
-                k3d cluster delete package-pipeline-devtest
-            else
-                k3d cluster delete package-pipeline-devtest
-                return 1
-            fi
-        else
-            k3d cluster delete package-pipeline-devtest
-            return 1
-        fi
-    else
-        k3d cluster delete package-pipeline-devtest
-        return 1
-    fi
-}
+    # Place kubernetes package test here
+    echo "Package install"
+    helm install devtest $1/chart -n devtest --create-namespace -f $1/tests/test-values.yml
+    kubectl wait --for=condition=available --timeout 600s -A deployment --all > /dev/null
+    kubectl wait --for=condition=ready --timeout 600s -A pods --all --field-selector status.phase=Running > /dev/null
+    echo "Package tests"
+    kubectl get ingress --all-namespaces
+    kubectl get all -A
+    cypress verify
+)}
 
 if [ -z $1 ]
 then
@@ -75,12 +39,18 @@ then
     exit 1
 fi
 
-if conftest $1 ; then
+conftest $1
+exit_status=$?
+if [ ${exit_status} -eq 0 ]; then
     echo "Conftest succeeded."
-    if packagetest $1 ; then
+    packagetest $1
+    exit_status=$?
+    if [ ${exit_status} -eq 0 ]; then
+        k3d cluster delete package-pipeline-devtest
         echo "Package tests succeeded."
         echo "Pipeline passed (2/2 stages passed)."
     else
+        k3d cluster delete package-pipeline-devtest
         echo "Package tests failed."
         echo "Pipeline failed (1/2 stages passed)."
     fi
