a60eb2fc38e62b31691dc46903e727a3a0d580bf
Josh Wolf <josh@joshwolf.dev>
2020-12-10 22:46:18 -0700

    new images

---
+++ scripts/package/gits.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p repos/
+
+# Clone core
+yq r "chart/values.yaml" "*.git.repo" | while IFS= read -r repo; do
+    git -C repos/ clone --no-checkout $repo
+done
+
+# Clone packages
+yq r "chart/values.yaml" "addons.*.git.repo" | while IFS= read -r repo; do
+    git -C repos/ clone --no-checkout $repo
+done
