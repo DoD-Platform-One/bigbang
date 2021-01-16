4e71f6aeda8279c390dc0c90cfa74f4098c88086
Ismail Ahmad <ismailahmad@ismailscbookpro.lan>
2021-01-15 23:15:59 -0500

    chore: testing tf_var change

--- .gitlab-ci/jobs/networking/aws/dependencies/terraform/env/dev/dev.tf
+++ .gitlab-ci/jobs/networking/aws/dependencies/terraform/env/dev/dev.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket               = "umbrella-tf-states"
-    key                  = "terraform.tfstate"
-    region               = "us-gov-west-1"
-    dynamodb_table       = "umbrella-tf-states-lock"
-    workspace_key_prefix = "aws-networking"
-  }
-}
 module "dev" {
   source   = "../../main"
   env      = "dev"
