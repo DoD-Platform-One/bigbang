d65250fb1046b70ed07074bc20c55ed4b4006b02
Josh Wolf <josh@joshwolf.dev>
2021-01-26 15:28:38 -0700

    ci: refactor ci to use dogfooding cluster

--- tests/bash/01_virtualservices.sh
+++ tests/bash/01_virtualservices.sh
@@ -3,9 +3,13 @@
 # exit on error
 set -e
 
+# Populate /etc/hosts
+ip=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
 echo "Checking "
 
 hosts=`kubectl get vs -A -o jsonpath="{ .items[*].spec.hosts[*] }"`
 for host in $hosts; do
+    echo "$ip $host" >> /etc/hosts
     curl -svv https://$host/ > /dev/null
 done
