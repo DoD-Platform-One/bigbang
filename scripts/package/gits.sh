4595a7ce70ab20c4f42ee858e27e2200b88bfa35
Micah Nagel <micah.nagel@parsons.com>
2021-03-15 21:24:14 +0000

    Resolve "Release pipeline always clones from `main`"

--- scripts/package/gits.sh
+++ scripts/package/gits.sh
@@ -4,11 +4,11 @@ set -ex
 mkdir -p repos/
 
 # Clone core
-yq r "chart/values.yaml" "*.git.repo" | while IFS= read -r repo; do
-    git -C repos/ clone --no-checkout $repo
+yq e ".*.git.repo | select(. != null) | path | .[-3] " "chart/values.yaml" | while IFS= read -r package; do
+  git -C repos/ clone -b $(yq e ".${package}.git.tag" "chart/values.yaml") $(yq e ".${package}.git.repo" "chart/values.yaml")
 done
 
-# Clone packages
-yq r "chart/values.yaml" "addons.*.git.repo" | while IFS= read -r repo; do
-    git -C repos/ clone --no-checkout $repo
+# Clone addons
+yq e ".addons.*.git.repo | select(. != null) | path | .[-3]" "chart/values.yaml" | while IFS= read -r package; do
+  git -C repos/ clone -b $(yq e ".addons.${package}.git.tag" "chart/values.yaml") $(yq e ".addons.${package}.git.repo" "chart/values.yaml")
 done
