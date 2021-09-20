91f54ae8183a971a1befa94dd57d5dc529bdc489
Mark Sanchez <mark.sanchez.8@us.af.mil>
2021-09-20 21:37:23 +0000

    SKIP UPGRADE: Resolve "Update CI pipeline to handle multi-ingress gateways and Keycloak"

--- tests/deploy/01_deploy_bigbang.sh
+++ tests/deploy/01_deploy_bigbang.sh
@@ -7,7 +7,6 @@ CI_VALUES_FILE="tests/ci/k3d/values.yaml"
 if [[ "${CI_COMMIT_BRANCH}" == "${CI_DEFAULT_BRANCH}" ]] || [[ ! -z "$CI_COMMIT_TAG" ]] || [[ $CI_MERGE_REQUEST_LABELS =~ "all-packages" ]]; then
   echo "all-packages label enabled, or on default branch or tag, enabling all addons"
   yq e ".addons.*.enabled = "true"" $CI_VALUES_FILE > tmpfile && mv tmpfile $CI_VALUES_FILE
-  yq e ".addons.keycloak.enabled = "false"" $CI_VALUES_FILE > tmpfile && mv tmpfile $CI_VALUES_FILE
 else
   IFS=","
   for package in $CI_MERGE_REQUEST_LABELS; do
@@ -21,6 +20,8 @@ fi
 # if keycloak enabled add ingress passthrough cert to addons.keycloak.ingress
 if [ "$(yq e ".addons.keycloak.enabled" "tests/ci/k3d/values.yaml")" == "true" ]; then
   yq eval-all 'select(fileIndex == 0) * select(filename == "tests/ci/keycloak-certs/keycloak-passthrough-values.yaml")' $CI_VALUES_FILE tests/ci/keycloak-certs/keycloak-passthrough-values.yaml > tmpfile && mv tmpfile $CI_VALUES_FILE
+#if keycloak is enabled add passthrough ingress gateway and gateway to istio.
+  yq eval-all 'select(filename == "tests/ci/k3d/values.yaml") * select(filename == "tests/ci/passthrough-gateway.yaml")' $CI_VALUES_FILE tests/ci/passthrough-gateway.yaml > tmpfile && mv tmpfile $CI_VALUES_FILE
 fi
 
 # Set controlPlaneCidr for ci-infra jobs which are RKE2
@@ -39,32 +40,16 @@ helm upgrade -i bigbang chart -n bigbang --create-namespace \
   --set registryCredentials[0].registry=registry1.dso.mil \
   -f ${CI_VALUES_FILE}
 
-# if keycloak is enabled use *.admin.bigbang.dev cert
-# otherwise use *.bigbang.dev
-if [ "$(yq e ".addons.keycloak.enabled" "tests/ci/k3d/values.yaml")" == "true" ]; then
-  # apply secrets kustomization pointing to current branch
-  if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
-    echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
-    kubectl apply -f tests/ci/keycloak.yaml
-  elif [ -z "$CI_COMMIT_TAG" ]; then
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
-    cat tests/ci/keycloak.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  else
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} tag"
-    # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
-    cat tests/ci/keycloak.yaml | sed 's|branch: master|tag: '"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  fi
+# apply secrets kustomization pointing to current branch or master if an upgrade job
+if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
+  echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
+  kubectl apply -f tests/ci/shared-secrets.yaml
+elif [ -z "$CI_COMMIT_TAG" ]; then
+  echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
+  cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
 else
-  # apply secrets kustomization pointing to current branch or master if an upgrade job
-  if [[ $(git branch --show-current) == "${CI_DEFAULT_BRANCH}" ]]; then
-    echo "Deploying secrets from the ${CI_DEFAULT_BRANCH} branch"
-    kubectl apply -f tests/ci/shared-secrets.yaml
-  elif [ -z "$CI_COMMIT_TAG" ]; then
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} branch"
-    cat tests/ci/shared-secrets.yaml | sed 's|master|'"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  else
-    echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} tag"
-    # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
-    cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
-  fi
+  echo "Deploying secrets from the ${CI_COMMIT_REF_NAME} tag"
+  # NOTE: $CI_COMMIT_REF_NAME = $CI_COMMIT_TAG when running on a tagged build
+  cat tests/ci/shared-secrets.yaml | sed 's|branch: master|tag: '"$CI_COMMIT_REF_NAME"'|g' | kubectl apply -f -
 fi
+
