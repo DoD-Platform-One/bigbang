ce06e2a37ff30db64629936a92b163aef94069b6
Micah Nagel <micah.nagel@parsons.com>
2021-10-26 17:03:19 +0000

    Update `wait_crd` to run on addons

--- tests/deploy/02_wait_for_helmreleases.sh
+++ tests/deploy/02_wait_for_helmreleases.sh
@@ -135,8 +135,8 @@ function wait_daemonset(){
 
 # Check for and run the wait_project function within <repo>/tests/wait.sh to wait for custom resources
 function wait_crd(){
-  yq e '. | keys | .[] | ... comments=""' "${VALUES_FILE}" | while IFS= read -r package; do
-    if [[ "$(yq e ".${package}.enabled" "${VALUES_FILE}")" == "true" ]]; then
+  yq e '(.,.addons) | .[] | ... comments="" | (path | join("."))' "${CI_VALUES_FILE}" | while IFS= read -r package; do
+    if [[ "$(yq e ".${package}.enabled" "${CI_VALUES_FILE}")" == "true" ]]; then
       gitrepo=$(yq e ".${package}.git.repo" "${VALUES_FILE}")
       version=$(yq e ".${package}.git.tag" "${VALUES_FILE}")
       if [[ -z "$version" || "$version" == "null" ]]; then
