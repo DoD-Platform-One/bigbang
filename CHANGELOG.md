5999a71b0c031ef85ea8a377189f0102033f6959
Micah Nagel <micah.nagel@parsons.com>
2021-12-01 21:42:29 +0000

    Add Wait on MetalLB

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ---
+## [2.0.20]
+### Added
+- New wait on MetalLB so that it is not listed in package image lists
+
 ## [2.0.19]
 ### Fixed
 - Kyverno to addons so it doesn't deploy for every pipeline
--- clusters/k3d/dependencies/metallb/install_metallb.sh
+++ clusters/k3d/dependencies/metallb/install_metallb.sh
@@ -6,3 +6,7 @@ set -x
 
 kubectl create -f ${PIPELINE_REPO_DESTINATION}/clusters/k3d/dependencies/metallb/metallb.yaml
 kubectl create -f ${PIPELINE_REPO_DESTINATION}/clusters/k3d/dependencies/metallb/metallb-config.yaml
+
+# Wait for MetalLB to be ready before proceeding
+kubectl rollout status daemonset speaker -n metallb-system
+kubectl rollout status deployment controller -n metallb-system
