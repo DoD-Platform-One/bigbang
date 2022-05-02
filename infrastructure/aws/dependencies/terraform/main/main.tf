241dcbebf1fa5b1134a7f6fb4f8c0cd1dc452366
Branden Cobb <cobb_branden@bah.com>
2022-05-02 19:09:52 +0000

    Moving aws transit gateways to bigbang-ci cluster

--- infrastructure/aws/dependencies/terraform/main/main.tf
+++ infrastructure/aws/dependencies/terraform/main/main.tf
@@ -126,20 +126,88 @@ resource "aws_security_group" "endpoints" {
   }
 }
 
+
 #
 # TGW Attachments
 # Attaches the management vpc (the hub) to the created vpc (the spokes).
 #
-module "spoke" {
-  source = "git::https://repo1.dso.mil/platform-one/big-bang/terraform-modules/spoke-tgw-attachments.git"
+data "aws_vpc" "spoke" {
+  id = module.vpc.vpc_id
+}
 
-  name       = local.name
-  hub_vpc_id = var.hub_vpc_id
-  hub_tgw    = var.hub_tgw
-  hub_tgw_rt = var.hub_tgw_rt
-  hub_tgwa   = var.hub_tgwa
+data "aws_vpc" "hub" {
+  id = var.hub_vpc_id
+}
 
-  spoke_vpc_id  = module.vpc.vpc_id
-  spoke_subnets = module.vpc.private_subnets
-  spoke_rt_ids  = module.vpc.private_route_table_ids
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-att" {
+  subnet_ids         = coalescelist(module.vpc.private_subnets, ["notset"])
+  vpc_id             = module.vpc.vpc_id
+  transit_gateway_id = var.hub_tgw
+
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+
+  tags = merge({
+    Name = "${local.name}-tgw-attachment"
+  }, var.common_tags)
+}
+
+// Associate routes from CLUSTER VPC to TGW RT
+resource "aws_ec2_transit_gateway_route_table_association" "tgw-rt-cluster-association" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att.id
+  transit_gateway_route_table_id = var.hub_tgw_rt
+}
+
+// Propogate routes from CLUSTER VPC to TGW RT
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw-rt-cluster-propagation" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.tgw-att.id
+  transit_gateway_route_table_id = var.hub_tgw_rt
+}
+
+// Add route to go from Cluster Private RT --> TGW --> bigbang-ci Private Subnets
+resource "aws_route" "cluster-private-to-bigbang-ci-private-route" {
+  count = length(module.vpc.private_route_table_ids)
+
+  route_table_id = module.vpc.private_route_table_ids[count.index]
+
+  destination_cidr_block = data.aws_vpc.hub.cidr_block
+  transit_gateway_id     = var.hub_tgw
+}
+
+// Add a route to go from bigbang-ci Private --> TGW --> Cluster Private
+data "aws_route_tables" "bigbang-ci-private-rts" {
+  vpc_id = data.aws_vpc.hub.id
+
+  filter {
+    name   = "tag:Name"
+    values = ["*private*"]
+  }
+}
+
+resource "aws_route" "bigbang-ci-private-to-cluster-private-route" {
+  count                     = length(data.aws_route_tables.bigbang-ci-private-rts.ids)
+
+  route_table_id            = tolist(data.aws_route_tables.bigbang-ci-private-rts.ids)[count.index]
+
+  destination_cidr_block    = data.aws_vpc.spoke.cidr_block
+  transit_gateway_id        = var.hub_tgw
+
+}
+
+// Add a route to go from bigbang-ci Public --> TGW --> Cluster Private
+// TODO: Evaluate if we actually want this...
+data "aws_route_table" "bigbang-ci-public-rt" {
+  vpc_id = data.aws_vpc.hub.id
+
+  filter {
+    name   = "tag:Name"
+    values = ["*public*"]
+  }
+}
+
+resource "aws_route" "bigbang-ci-public-to-cluster-private-route" {
+  route_table_id = data.aws_route_table.bigbang-ci-public-rt.id
+
+  destination_cidr_block = data.aws_vpc.spoke.cidr_block
+  transit_gateway_id     = var.hub_tgw
 }
