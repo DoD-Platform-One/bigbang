990ed70154a3548955b0da5bf01eea777e64850b
Cody Williams <cody.williams.5@us.af.mil>
2022-08-22 15:10:11 +0000

    updating readme deps

--- library/templates.sh
+++ library/templates.sh
@@ -1629,8 +1629,11 @@ get_cpumem(){
 
 renovate_download_external_deps() {
   curl -sO https://repo1.dso.mil/platform-one/big-bang/apps/library-charts/gluon/-/raw/master/docs/README.md.gotmpl -o "${CI_PROJECT_DIR}"/scripts/README.md.gotmpl
-
+  
   curl -sO https://repo1.dso.mil/platform-one/big-bang/apps/library-charts/gluon/-/raw/master/docs/.helmdocsignore -o "${CI_PROJECT_DIR}"/scripts/.helmdocsignore
+
+  curl -sO https://repo1.dso.mil/platform-one/big-bang/apps/library-charts/gluon/-/raw/master/docs/_templates.gotmpl -o "${CI_PROJECT_DIR}"/scripts/_templates.gotmpl
 
   mkdir -p "${CI_PROJECT_DIR}"/renovate
 }
+
