d55c0c8e8fa1ccb28c2225fbb37483bc053c8b38
razzle <harry.randazzo@us.af.mil>
2022-08-25 10:23:13 -0500

    update to only use chrome in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,59 +46,68 @@ const config: PlaywrightTestConfig = {
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: {
-        ...devices["Desktop Chrome"],
-      },
-    },
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: {
+            ...devices["Desktop Chrome"],
+          },
+        },
+      ]
+    : [
+        {
+          name: "firefox",
+          use: {
+            ...devices["Desktop Firefox"],
+          },
+        },
 
-    {
-      name: "firefox",
-      use: {
-        ...devices["Desktop Firefox"],
-      },
-    },
+        {
+          name: "firefox",
+          use: {
+            ...devices["Desktop Firefox"],
+          },
+        },
 
-    // {
-    //   name: "webkit",
-    //   use: {
-    //     ...devices["Desktop Safari"],
-    //   },
-    // },
+        // {
+        //   name: "webkit",
+        //   use: {
+        //     ...devices["Desktop Safari"],
+        //   },
+        // },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
+        /* Test against mobile viewports. */
+        // {
+        //   name: "Mobile Chrome",
+        //   use: {
+        //     ...devices["Pixel 5"],
+        //   },
+        // },
+        // {
+        //   name: "Mobile Safari",
+        //   use: {
+        //     ...devices["iPhone 12"],
+        //   },
+        // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
-  ],
+        /* Test against branded browsers. */
+        // {
+        //   name: "Microsoft Edge",
+        //   use: {
+        //     channel: "msedge",
+        //   },
+        // },
+        // {
+        //   name: "Google Chrome",
+        //   use: {
+        //     channel: "chrome",
+        //   },
+        // },
+      ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: "test-results/",
 
   /* Run your local dev server before starting the tests */
   // webServer: {
