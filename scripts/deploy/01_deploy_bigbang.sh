99ab6d0a68e326f5268de61119ed3e8bec90f96d
Micah Nagel <micah.nagel@parsons.com>
2022-09-29 18:44:28 +0000

    Revert "Merge branch 'add-kyvernoreporter' into 'master'"
    This reverts merge request !262

--- scripts/deploy/01_deploy_bigbang.sh
+++ scripts/deploy/01_deploy_bigbang.sh
@@ -29,12 +29,6 @@ if [[ "${PIPELINE_TYPE}" == "BB" ]]; then
       yq e ".kyvernopolicies.enabled = "true"" $CI_VALUES_FILE > tmpfile && mv tmpfile $CI_VALUES_FILE
   fi
 
-  # Enable kyverno reporter
-  if [[ "${CI_DEPLOY_LABELS[*]}" =~ "kyvernoreporter" ]] || [[ "${CI_COMMIT_BRANCH}" == "${CI_DEFAULT_BRANCH}" ]] || [[ ! -z "$CI_COMMIT_TAG" ]] || [[ ${CI_DEPLOY_LABELS[*]} =~ "all-packages" ]]; then
-      echo "Enabling kyverno reporter"
-      yq e ".kyvernoreporter.enabled = "true"" $CI_VALUES_FILE > tmpfile && mv tmpfile $CI_VALUES_FILE
-  fi
-
   # Enable tempo
   if [[ "${CI_DEPLOY_LABELS[*]}" =~ "tempo" ]] || [[ "${CI_COMMIT_BRANCH}" == "${CI_DEFAULT_BRANCH}" ]] || [[ ! -z "$CI_COMMIT_TAG" ]] || [[ ${CI_DEPLOY_LABELS[*]} =~ "all-packages" ]]; then
       echo "Enabling tempo"
