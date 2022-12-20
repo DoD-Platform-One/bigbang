04a91bb07b0c68991278c7dec97c5bcff75d035a
evan.rush <rush_evan@bah.com>
2022-12-20 17:52:34 +0000

    Resolve "Create IaC and Pipeline for Air Gap Infrastructure"

---
+++ clusters/airgap/dependencies/terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "ci_pipeline_url" {
+  type        = string
+  default     = "ci_pipeline_url"
+}
+
+variable "ci_pipeline_id" {
+  type        = string
+  default     = "ci_pipeline_id"
+}
+
+variable "airgap_env_name" {
+  type        = string
+  default     = "airgap_env_name"
+}
+
+variable "env" {}
