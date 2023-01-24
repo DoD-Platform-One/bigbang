1925042ad3896ef34685553d24ea7199e7252ad9
evan.rush <rush_evan@bah.com>
2023-01-24 21:11:58 +0000

    Airgap Pipeline

--- clusters/airgap/dependencies/terraform/main.tf
+++ clusters/airgap/dependencies/terraform/main.tf
@@ -36,11 +36,15 @@ resource random_id index {
 
 resource "aws_instance" "bb-ci-airgap" {
   ami                    = data.aws_ami.airgap_ami.id
-  instance_type          = "m5d.2xlarge"
+  instance_type          = "m5.2xlarge"
   subnet_id              = local.random_subnet_id
   vpc_security_group_ids = [aws_security_group.airgap_allow_ci.id]
   iam_instance_profile   = "airgap_ci_role"
-  key_name               = "rush-packer-test"
+  key_name               = var.env
+  root_block_device {
+    volume_size           = "100"
+    delete_on_termination = true
+  }
   tags = {
     Name            = var.airgap_env_name
     ci_pipeline_url = var.ci_pipeline_url
@@ -48,7 +52,11 @@ resource "aws_instance" "bb-ci-airgap" {
   }
   user_data = <<EOF
 #!/bin/bash
-sleep 15
+sleep 5
+mkdir ~/registry
+/usr/local/bin/aws s3 cp s3://umbrella-bigbang-releases/umbrella/${var.bb_release}/images.tar.gz ~/
+tar -xvf ~/images.tar.gz -C ~/registry
+# inject ip for registry mirror
 LOCALIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 sed -i "s/LOCALIPHERE/$LOCALIP/g" /etc/rancher/k3s/registries.yaml
 cat - << REGISTRYEOF > /etc/docker/daemon.json
@@ -58,14 +66,31 @@ cat - << REGISTRYEOF > /etc/docker/daemon.json
 REGISTRYEOF
 systemctl restart docker
 sleep 5
-docker run -d -p 5000:5000 --restart=always --name registry registry:2
+docker run -d \
+  -p 5000:5000 \
+  -v ~/registry/var/lib/registry:/var/lib/registry \
+  --restart=always \
+  --name registry registry:2
 INSTALL_K3S_SKIP_DOWNLOAD=true \
   INSTALL_K3S_EXEC="--disable=traefik" \
   INSTALL_K3S_SELINUX_WARN=true \
   INSTALL_K3S_SKIP_SELINUX_RPM=true \
   ./k3s-install.sh
+# verify k3s is ready
 sleep 30
 KUBECONFIG=/etc/rancher/k3s/k3s.yaml kubectl wait --for=condition=Ready --timeout=120s pods --all --all-namespaces
+# create tiny git server
+useradd --create-home --shell /bin/bash git
+ssh-keygen  -b 4096 -t rsa -f /git.pem -q -N ""
+mkdir -p /home/git/.ssh && chmod 700 /home/git/.ssh/
+touch /home/git/.ssh/authorized_keys && chmod 600 /home/git/.ssh/authorized_keys
+cat /git.pem.pub >> /home/git/.ssh/authorized_keys
+# repo prep
+/usr/local/bin/aws s3 cp s3://umbrella-bigbang-releases/umbrella/${var.bb_release}/repositories.tar.gz ~/
+tar -xvf ~/repositories.tar.gz -C /home/git/
+chown -R git:users /home/git/
+/usr/local/bin/aws s3 cp /git.pem s3://airgap-tf-states/airgap/${var.env}/git.pem
+# inject ip into kuberconfig for external access
 sed -i "s/127.0.0.1/$LOCALIP/g" /etc/rancher/k3s/k3s.yaml
 /usr/local/bin/aws s3 cp /etc/rancher/k3s/k3s.yaml s3://airgap-tf-states/airgap/${var.env}/airgap_kubeconfig.yaml
 EOF
