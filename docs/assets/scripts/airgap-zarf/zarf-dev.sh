ad17bf9fc7b2992a21f40bf5a16e54bbd9bb598f
Danny Gershman <danny@radiusmethod.com>
2023-04-03 15:04:40 +0000

    Zarf development script and documentation

---
+++ docs/assets/scripts/airgap-zarf/zarf-dev.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ZARF_VERSION=v0.25.0
+
+# Prerequisites: REGISTRY1_USERNAME and REGISTRY1_PASSWORD must be exported locally.
+# Configurable: ZARF_TEST_REPO, ZARF_TEST_REPO_BRANCH, ZARF_TEST_REPO_DIRECTORY all define where to pick up the zarf.yaml file.
+# Example with configuration: KeyName=<KeyName> PublicIP=<Ip> ZARF_TEST_REPO=https://repo1.dso.mil/some-repo.git ZARF_TEST_REPO_BRANCH=development docs/assets/scripts/airgap-zarf/zarf-dev.sh
+
+ZARF_TEST_REPO=${ZARF_TEST_REPO:=https://github.com/defenseunicorns/zarf}
+ZARF_TEST_REPO_BRANCH=${ZARF_TEST_REPO_BRANCH:=main}
+ZARF_TEST_REPO_DIRECTORY=${ZARF_TEST_REPO_DIRECTORY:=zarf/examples/big-bang}
+
+function run() {
+  ssh -i ~/.ssh/${KeyName}.pem -o StrictHostKeyChecking=no -o IdentitiesOnly=yes ubuntu@${PublicIP} $1
+}
+
+# install zarf
+echo Installing zarf...
+run "curl -LO https://github.com/defenseunicorns/zarf/releases/download/${ZARF_VERSION}/zarf_${ZARF_VERSION}_Linux_amd64"
+run "sudo mv /home/ubuntu/zarf_${ZARF_VERSION}_Linux_amd64 /usr/local/bin/zarf"
+run "sudo chmod +x /usr/local/bin/zarf"
+
+# get zarf init package
+echo "Retrieving zarf init package..."
+run "wget -q https://github.com/defenseunicorns/zarf/releases/download/${ZARF_VERSION}/zarf-init-amd64-${ZARF_VERSION}.tar.zst"
+
+# zarf init, package and deploy
+run "set +o history && echo ${REGISTRY1_PASSWORD} | zarf tools registry login registry1.dso.mil --username ${REGISTRY1_USERNAME} --password-stdin || set -o history"
+run "zarf init --components=git-server --confirm"
+run "git clone --single-branch --branch ${ZARF_TEST_REPO_BRANCH} ${ZARF_TEST_REPO}"
+run "cd ${ZARF_TEST_REPO_DIRECTORY} && zarf package create --confirm --max-package-size=0"
+run "cd ${ZARF_TEST_REPO_DIRECTORY} && zarf package deploy zarf-package-big-bang-example-amd64-1.54.0.tar.zst --confirm --components=gitea-virtual-service"
