66e73f3aea5c1982f364a0cfb6cfbb993b124926
Danny Gershman <danny@radiusmethod.com>
2023-04-04 14:57:12 +0000

    Override k8s version for k3d

--- docs/assets/scripts/developer/k3d-dev.sh
+++ docs/assets/scripts/developer/k3d-dev.sh
@@ -434,6 +434,12 @@ k3d_command+=" --k3s-arg \"--disable=traefik@server:0\"  --k3s-arg \"--disable=m
 # Port mappings to support Istio ingress + API access
 k3d_command+=" --port 80:80@loadbalancer --port 443:443@loadbalancer --api-port 6443"
 
+K3S_IMAGE_TAG=${K3S_IMAGE_TAG:=""}
+if [[ ! -z "$K3S_IMAGE_TAG" ]]; then
+  echo "Using custom K3S image tag $K3S_IMAGE_TAG..."
+  k3d_command+=" --image docker.io/rancher/k3s:$K3S_IMAGE_TAG"
+fi
+
 # Add MetalLB specific k3d config
 if [[ "$METAL_LB" == true ]]; then
   # create docker network for k3d cluster
