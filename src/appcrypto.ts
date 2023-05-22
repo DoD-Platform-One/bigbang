64354c033fde8d5c1eba60ff4051f7699014ed0a
Alfredo Diaz <alfredo.diaz@omnifederal.com>
2023-05-22 21:41:16 +0000

    Resolve "Feature: Send Confirmation Comment Back to GitHub on PR Mirror"

--- src/appcrypto.ts
+++ src/appcrypto.ts
@@ -2,18 +2,37 @@ import crypto from 'crypto'
 import {Response, NextFunction} from 'express'
 import { AppRequest } from './app';
 import dotenv from 'dotenv'
+import fs from 'fs'
+import jwt from 'jsonwebtoken'
 dotenv.config();
 
 export const githubSigHeaderName = 'X-Hub-Signature-256';
 export const gitlabTokenHeaderName = 'X-Gitlab-Token';
 export const adminTokenHeaderName = 'X-Admin-Token';
 
+const privateKey = fs.readFileSync('privatekey.pem');
+
 const sigPrefix = 'sha256='; //set this to your signature prefix if any
 const gitHubSecret = process.env.GITHUB_WEBHOOK_SECRET ?? "";
 const gitLabSecret = process.env.GITLAB_WEBHOOK_SECRET ?? "";
 const adminDefaultSecret = ""
 const adminSecret = process.env.ADMIN_TOKEN ?? adminDefaultSecret;
+const appId = process.env.APP_ID ?? "";
 
+
+export const signPayloadJWT = () => {
+  
+  const payload = {
+    // Issued at time
+    iat: Math.floor(Date.now() / 1000),
+    // JWT expiration time (10 minutes maximum)
+    exp: Math.floor(Date.now() / 1000) + 600,
+    // GitHub App's identifier
+    iss: appId
+  };
+
+  return jwt.sign(payload, privateKey, { algorithm: 'RS256'});
+}
 
 export const processWebHookSignature = function (body: any , signature: any) {
   const hmac = crypto.createHmac('SHA256', gitHubSecret)
